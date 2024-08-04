ba96a8b4c97a1190983ee5e61b409e45a3a5fb45
sobir-t <sobirdzhon.t@gmail.com>
2024-08-03 23:53:08 -0500

    initial setup

---
+++ playwright.config.ts
@@ -0,0 +1,118 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  expect: {
+    timeout: 10 * 1000,
+  },
+  // globalTimeout: 60 * 60 * 1000,
+  // Glob patterns or regular expressions to ignore test files.
+  // testIgnore: '*test-assets',
+  // Glob patterns or regular expressions that match test files.
+  // testMatch: '*todo-tests/*.spec.ts',
+  testIgnore: 'example*.spec.ts',
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+
+    // Maximum time each action such as `click()` can take. Defaults to 0 (no limit).
+    actionTimeout: 0,
+
+    // Name of the browser that runs tests. For example `chromium`, `firefox`, `webkit`.
+    browserName: 'chromium',
+    // viewport: { width: 100, height: 100 },
+
+    // Toggles bypassing Content-Security-Policy.
+    bypassCSP: true,
+
+    // Channel to use, for example "chrome", "chrome-beta", "msedge", "msedge-beta".
+    channel: 'chrome',
+
+    // Run browser in headless mode.
+    headless: false,
+
+    // Change the default data-testid attribute.
+    // testIdAttribute: 'pw-test-id',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'Smoke',
+      testMatch: '*smoke*.spec.ts',
+      retries: 0,
+    },
+    {
+      name: 'chromium',
+      testIgnore: '*mobile.spec.ts',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      testIgnore: '*mobile.spec.ts',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      testIgnore: '*mobile.spec.ts',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      testMatch: '*mobile.spec.ts',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      testMatch: '*mobile.spec.ts',
+      use: { ...devices['iPhone 12'] },
+    },
+
+    /* Test against branded browsers. */
+    {
+      name: 'Microsoft Edge',
+      testIgnore: '*mobile.spec.ts',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+    {
+      name: 'Google Chrome',
+      testIgnore: '*mobile.spec.ts',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
