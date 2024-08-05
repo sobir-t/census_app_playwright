b8aeb02fd2082d3c9452a22534774ed010c386ed
sobir-t <sobirdzhon.t@gmail.com>
2024-08-04 21:38:12 -0500

    added pm2 ecosystem and script to test env

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+require('@dotenvx/dotenvx').config();
+
+module.exports = {
+  apps: [
+    {
+      name: 'pr',
+      script: 'npx ts-node script.ts',
+      env_production: {
+        NODE_ENV: 'production',
+        TEST_USER_EMAIL: process.env.TEST_USER_EMAIL,
+        TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: process.env.SSH_KEY,
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ssh_options: 'ForwardAgent=yes',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+};
---
+++ sctipt.ts
@@ -0,0 +1,4 @@
+console.log(`process.env.TEST_USER_EMAIL = ${process.env.TEST_USER_EMAIL}`);
+console.log(
+  `process.env.TEST_USER_PASSWORD = ${process.env.TEST_USER_PASSWORD}`
+);
